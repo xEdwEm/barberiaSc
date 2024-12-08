c5c357e0bbd8272793e1f0e00d674641b175d019
xEdwEm <144190868+xEdwEm@users.noreply.github.com>
2024-12-07 22:14:57 -0400

    Add files via upload

---
+++ Includes/templates/footer.php
@@ -0,0 +1,33 @@
+		<footer class="footer_section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 xs-padding">
+						<div class="copyright">
+						</div>
+					</div>
+					<div class="col-md-6 xs-padding">
+						<ul class="footer_social">
+							<li><a href="appointment.php">Órdenes</a></li>
+							<li><a href=>Términos</a></li>
+							<li><a href="index.php#contact-us">Reportar un Problema</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+
+		<!-- INCLUDE JS SCRIPTS -->
+
+		<script src="Design/js/jquery.min.js"></script>
+		<script src="Design/js/bootstrap.min.js"></script>
+		<script src="Design/js/bootstrap.bundle.min.js"></script>
+		<script src="Design/js/main.js"></script>
+
+		</body>
+
+		<!-- END BODY TAG -->
+
+		</html>
+
+		<!-- END HTML TAG -->
